6346a6ce7178c16a0207519f6d848fc1d9867af9
Emil <raysloks@gmail.com>
2020-10-07 12:34:02 +0200

    added prototype wall and mob shadows

---
+++ Assets/Scripts/ShadowWallToggle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class ShadowWallToggle : MonoBehaviour
+{
+    private ShadowCaster2D shadow;
+    private Player player;
+
+    private void Awake()
+    {
+        shadow = GetComponent<ShadowCaster2D>();
+        player = FindObjectOfType<Player>();
+    }
+
+    private void Update()
+    {
+        shadow.selfShadows = player.transform.position.y > transform.position.y;
+    }
+}
