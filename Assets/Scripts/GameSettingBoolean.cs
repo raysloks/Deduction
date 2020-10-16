222355300407ca6ea614c18235d8f3ba3f490223
Emil <raysloks@gmail.com>
2020-10-16 15:22:32 +0200

    added boolean settings

---
+++ Assets/Scripts/GameSettingBoolean.cs
@@ -0,0 +1,14 @@
+﻿
+
+public class GameSettingBoolean : GameSetting
+{
+    public override string Get()
+    {
+        return value.ToString();
+    }
+
+    public override void Set(string text)
+    {
+        value = long.Parse(text);
+    }
+}
--- Assets/Scripts/GameSettings.cs
+++ Assets/Scripts/GameSettings.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class GameSettings : MonoBehaviour
 {
@@ -9,13 +10,15 @@ public class GameSettings : MonoBehaviour
         new GameSettingInteger{ name = "Impostor Count", value = 1 },
         new GameSettingTime{name = "Kill Cooldown" , value = 30000000000 },
         new GameSettingTime{name = "Vote Time" , value = 30000000000 },
-        new GameSettingTime{name = "Discussion Time" , value = 90000000000 }
+        new GameSettingTime{name = "Discussion Time" , value = 90000000000 },
+        new GameSettingBoolean{name = "Test", value = 0 }
     };
 
     public RectTransform window;
-    public GameObject prefab;
+    public GameObject textFieldPrefab;
+    public GameObject togglePrefab;
 
-    private List<InputField> inputFields = new List<InputField>();
+    private List<Selectable> inputs = new List<Selectable>();
 
     public GameController controller;
 
@@ -24,28 +27,49 @@ public class GameSettings : MonoBehaviour
         for (int i = 0; i < settings.Count; ++i)
         {
             GameSetting setting = settings[i];
-            var go = Instantiate(prefab, window);
-            go.GetComponentInChildren<Text>().text = setting.name;
-            var inputField = go.GetComponentInChildren<InputField>();
-            inputField.text = setting.Get();
+            Selectable input;
+            GameObject go;
             int index = i;
-            inputField.onEndEdit.AddListener((string text) =>
+            if (setting is GameSettingBoolean boolean)
             {
-                long prev = setting.value;
-                setting.Set(text);
-                if (prev != setting.value)
+                go = Instantiate(togglePrefab, window);
+                var toggle = go.GetComponentInChildren<Toggle>();
+                toggle.isOn = setting.value != 0;
+                toggle.onValueChanged.AddListener((bool value) =>
                 {
+                    setting.value = value ? 1 : 0;
                     GameSettingSet message;
                     message.setting = (ushort)index;
                     message.value = setting.value;
                     controller.handler.link.Send(message);
-                }
-                else
+                });
+                input = toggle;
+            }
+            else
+            {
+                go = Instantiate(textFieldPrefab, window);
+                var inputField = go.GetComponentInChildren<InputField>();
+                inputField.text = setting.Get();
+                inputField.onEndEdit.AddListener((string text) =>
                 {
-                    inputField.SetTextWithoutNotify(setting.Get());
-                }
-            });
-            inputFields.Add(inputField);
+                    long prev = setting.value;
+                    setting.Set(text);
+                    if (prev != setting.value)
+                    {
+                        GameSettingSet message;
+                        message.setting = (ushort)index;
+                        message.value = setting.value;
+                        controller.handler.link.Send(message);
+                    }
+                    else
+                    {
+                        inputField.SetTextWithoutNotify(setting.Get());
+                    }
+                });
+                input = inputField;
+            }
+            go.GetComponentInChildren<Text>().text = setting.name;
+            inputs.Add(input);
         }
     }
 
@@ -57,6 +81,15 @@ public class GameSettings : MonoBehaviour
     public void SetSetting(int setting, long value)
     {
         settings[setting].value = value;
-        inputFields[setting].SetTextWithoutNotify(settings[setting].Get());
+        UpdateInputDisplay(setting);
+    }
+
+    private void UpdateInputDisplay(int setting)
+    {
+        var input = inputs[setting];
+        if (input is InputField inputField)
+            inputField.SetTextWithoutNotify(settings[setting].Get());
+        if (input is Toggle toggle)
+            toggle.SetIsOnWithoutNotify(settings[setting].value != 0);
     }
 }
