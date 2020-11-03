800afb5ee698c7560a0cc8664e11e755d9bacec6
Muguai <37656342+Muguai@users.noreply.github.com>
2020-11-03 18:28:36 +0100

    Hilariouos Minigame added LOL

---
+++ Assets/Scripts/Minigame/Minigame11/ActivateFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateFollow : MonoBehaviour
+{
+    private GameObject container;
+    public GameObject image1;
+    public GameObject image2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        container = transform.parent.gameObject;
+        if (container.GetComponent<MinigameHolder>().stayCloseToTarget.GetComponent<StayCloseToTarget>().getIsDone() == true)
+        {
+            container.GetComponent<MinigameHolder>().stayCloseToTarget.SetActive(false);
+            image2.SetActive(true);
+            FindObjectOfType<MinigamePopupScript>().MinigameWon();
+        }
+        else
+        {
+            image1.SetActive(true);
+            container.GetComponent<MinigameHolder>().stayCloseToTarget.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Minigame/Minigame11/StayCloseToTarget.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StayCloseToTarget : MonoBehaviour
+{
+    List<GameObject> mobs = new List<GameObject>();
+    public float sliderGain = 0.1f;
+    private Vector3 orignialScale;
+    private bool isDone = false;
+    private Transform smallbar;
+    private Player player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        smallbar = transform.GetChild(0).gameObject.transform.GetChild(0);
+        orignialScale = smallbar.localScale;
+        player = transform.parent.gameObject.GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(mobs.Count > 0 && !isDone)
+        {
+            if (player.canMove == true)
+            {
+                smallbar.localScale += new Vector3((sliderGain * Time.deltaTime), 0f, 0f);
+                if (smallbar.localScale.x >= 2f)
+                {
+                    Debug.Log("Done");
+                    isDone = true;
+                }
+            }         
+        }
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Mob" && mobs.Contains(col.gameObject) == false)
+        {
+            Debug.Log("BOOYAH ENTER");
+            mobs.Add(col.gameObject);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.tag == "Mob" && mobs.Contains(col.gameObject) == true)
+        {
+            Debug.Log("BOOYAH EXIT");            
+            mobs.Remove(col.gameObject);
+            if (mobs.Count == 0 && isDone == false)
+            {
+                smallbar.localScale = orignialScale;
+            }
+        }
+    }
+
+    public bool getIsDone()
+    {
+        return isDone;
+    }
+}
