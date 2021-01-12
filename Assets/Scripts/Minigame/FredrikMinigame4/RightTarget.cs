dd49b95d0fa1e41ba3231e4ca6044a6024b66eba
Muguai <37656342+Muguai@users.noreply.github.com>
2021-01-12 09:34:06 +0100

    ShootMinigameFixed

--- Assets/Scripts/Minigame/FredrikMinigame4/RightTarget.cs
+++ Assets/Scripts/Minigame/FredrikMinigame4/RightTarget.cs
@@ -11,8 +11,28 @@ public class RightTarget : MonoBehaviour
     public GameObject textObject;
 
     public List<AudioClip> correctSounds;
-
+    /*
     void OnMouseDown()
+    {
+        GameObject d = Instantiate(deathParticle, transform.position, Quaternion.identity);
+        GameObject t = Instantiate(textObject, transform.position, Quaternion.identity);
+
+        t.transform.SetParent(transform.parent);
+        d.transform.SetParent(transform.parent);
+
+        SoundEvent se = new SoundEvent();
+        se.UnitSound = correctSounds;
+        se.UnitGameObjectPos = transform.position;
+        EventCallbacks.EventSystem.Current.FireEvent(EVENT_TYPE.PLAY_SOUND, se);
+
+        DestroyImmediate(gameObject, true);
+
+        // Destroy the gameObject after clicking on it
+        Debug.Log("Right");
+    }
+    */
+
+    public void Death()
     {
         GameObject d = Instantiate(deathParticle, transform.position, Quaternion.identity);
         GameObject t = Instantiate(textObject, transform.position, Quaternion.identity);
--- Assets/Scripts/Minigame/FredrikMinigame4/WrongTarget.cs
+++ Assets/Scripts/Minigame/FredrikMinigame4/WrongTarget.cs
@@ -10,8 +10,28 @@ public class WrongTarget : MonoBehaviour
     public GameObject textObject;
 
     public List<AudioClip> wrongSounds;
-
+    /*
     void OnMouseDown()
+    {
+        GameObject d = Instantiate(deathParticle, transform.position, Quaternion.identity);
+        GameObject t = Instantiate(textObject, transform.position, Quaternion.identity);
+
+        t.transform.SetParent(transform.parent);
+        d.transform.SetParent(transform.parent);
+        SoundEvent se = new SoundEvent();
+        se.UnitSound = wrongSounds;
+        se.UnitGameObjectPos = transform.position;
+        EventCallbacks.EventSystem.Current.FireEvent(EVENT_TYPE.PLAY_SOUND, se);
+
+
+        DestroyImmediate(gameObject, true);
+
+        // Destroy the gameObject after clicking on it
+        Debug.Log("Wrong");
+    }
+    */
+
+    public void Death()
     {
         GameObject d = Instantiate(deathParticle, transform.position, Quaternion.identity);
         GameObject t = Instantiate(textObject, transform.position, Quaternion.identity);
